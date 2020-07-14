88c042e77843c901d4df27a9ea5a5abf7b1079fb
Samir Triande <habib.triande@gmail.com>
2020-07-14 09:56:50 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,15 @@
 # Write your solution here
+# first solution
+current_time = Time.now.to_i
+#if current_time.even?
+  #puts "Even!"
+#else
+  #puts "Odd!"
+#end
+
+# second solution using the modulo operator to determine if even or odd
+if current_time % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
+end
